6f0ed640f11890e8d8063e7f012a744998d516c0
Robert Zaleski <rzaleski@franciscan.edu>
2024-02-25 10:33:14 -0500

    Added .env.template and made index.ts use it.

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,5 +1,7 @@
 import {Elysia} from 'elysia'
 
+const PORT = process.env['LISTEN_PORT'];
+console.log(`Listening on port ${PORT}`);
 new Elysia()
   .get('/', () => 'Hello Elysia')
-  .listen(3000)
+  .listen(PORT);
