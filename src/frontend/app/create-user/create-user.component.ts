5abef98e27297b0d5cd0d5535ec9ab8fb6e68768
Nicholas Balarezo <nbalarezo001@student.@franciscan.edu>
2024-03-23 23:10:37 -0400

    added conent to create-user.component.ts, including save() function

--- src/frontend/app/create-user/create-user.component.ts
+++ src/frontend/app/create-user/create-user.component.ts
@@ -1,12 +1,57 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User } from "../../../models/user";
+import { UserService } from '../services/user.service';
+//import { CreatePosts } from '../create-posts/create-posts.component';
 
 @Component({
   selector: 'ng-create-user',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.css'
 })
+
 export class CreateUserComponent {
 
+  constructor(private userService: UserService, private router: Router) {}
+
+  firstName: string = '';
+  lastName: string = '';
+  dob: string = '';
+  gender: string = '';
+  description: string = '';
+  profilePicture: string = '';
+  interest1: string = '';
+  interest2: string = '';
+  interest3: string = '';
+
+  save(): void {
+    const toSave: User = {
+      firstName: this.lastName,
+      lastName: this.lastName,
+      dob: this.dob,
+      gender: this.gender,
+    }
+    if (this.description) {
+      toSave.description = this.description;
+    }
+    if (this.profilePicture) {
+      toSave.profilePicture = this.profilePicture;
+    }
+    if (this.interest1) {
+      toSave.interest1 = this.interest1;
+    }
+    if (this.interest2) {
+      toSave.interest2 = this.interest2;
+    }
+    if (this.interest3) {
+      toSave.interest3 = this.interest3;
+    }
+    this.userService.createUser(toSave).subscribe(user => {
+      console.log('Saved ',user,', returning home.');
+      this.router.navigate(['/']);
+    });
+  }
 }
