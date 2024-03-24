81a7151d798af88cd56e10838844954ec809427e
Nicholas Balarezo <nbalarezo001@student.@franciscan.edu>
2024-03-23 23:18:25 -0400

    added route for create-user component

--- src/frontend/app/app.routes.ts
+++ src/frontend/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CreateUserComponent } from './create-user/create-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'create-user',
+    component: CreateUserComponent,
+  },
+
+];
