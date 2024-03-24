1e6e322be55c509521888ef8b7fb1996411e3029
Nicholas Balarezo <nbalarezo001@student.@franciscan.edu>
2024-03-23 23:17:38 -0400

    added RouterModule import to app.comppnent.ts

--- src/frontend/app/app.component.ts
+++ src/frontend/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'ng-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
