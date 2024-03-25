cccd95add632da68daee3c089998f33b4118d116
Martinez, Joernie <jmartinez005@student.franciscan.edu>
2024-03-24 22:34:59 -0400

    made posts service to store posts objects in list

--- src/frontend/app/services/posts.service.ts
+++ src/frontend/app/services/posts.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Posts } from '../../../models/posts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  private posts: Posts[] = [];
+
+  constructor() {}
+
+  createPost(post: Posts): void {
+    this.posts.push(post);
+  }
+
+  getAllPosts(): Posts[] {
+    return this.posts;
+  }
+
 }
