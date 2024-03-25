56da46156a368902973b3bd11d3a52758b63d1c0
Nicholas Balarezo <nbalarezo001@student.@franciscan.edu>
2024-03-25 11:02:23 -0400

    resolved merge conflicts

--- src/frontend/app/services/user.service.ts
+++ src/frontend/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from '../../../models/user';
 import { Observable, of } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,10 @@ import { Observable, of } from 'rxjs';
 export class UserService {
 
   // TODO Connect to the webserver and delete this.
-  users: User[] = [{
-    firstName: 'Kyle',
-    lastName: 'Crocodile',
-    gender: 'Male',
-    dob: new Date('03/25/1900'),
-  }];
+  users: User[] = [
+    {firstName: 'Kyle', lastName: 'Crocodile', gender: 'Male', dob: new Date('03/25/1900')},
+    {firstName: 'Jim', lastName: 'Flim', gender: 'Male', dob: new Date('07/53/1943')}
+  ];
   nextId: number = 1;
   constructor() { }
 
