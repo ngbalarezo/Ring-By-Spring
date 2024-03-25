2933d8c140347b902b3633df280817a358467229
Martinez, Joernie <jmartinez005@student.franciscan.edu>
2024-03-24 22:59:41 -0400

    added posts route

--- src/frontend/app/app.routes.ts
+++ src/frontend/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { CreateUserComponent } from './create-user/create-user.component';
-import { CreateUserComponent } from './create-user/create-user.component';
+import { CreatePostsComponent } from './create-posts/create-posts.component';
 
 export const routes: Routes = [
   {
@@ -10,8 +10,8 @@ export const routes: Routes = [
 
 
   {
-    path: 'create-user',
-    component: CreateUserComponent,
+    path: 'create-post',
+    component: CreatePostsComponent,
   },
 
 ];
