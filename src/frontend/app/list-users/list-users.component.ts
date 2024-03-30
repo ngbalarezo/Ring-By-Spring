11cd7d768d264b14aef8ab7928d3b0ff76be56ab
Nicholas Balarezo <nbalarezo001@student.@franciscan.edu>
2024-03-30 16:46:02 -0400

    connected service to component, added content to component and html file.

--- src/frontend/app/list-users/list-users.component.ts
+++ src/frontend/app/list-users/list-users.component.ts
@@ -3,14 +3,19 @@ import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { User } from "../../../models/user";
 import { UserService } from '../services/user.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'ng-list-users',
   standalone: true,
-  imports: [],
+  imports: [UserService, NgFor],
   templateUrl: './list-users.component.html',
   styleUrl: './list-users.component.css'
 })
 export class ListUsersComponent {
+  
+  constructor(private userService: UserService, private router: Router) {}
+
+  users = this.userService.users;
 
 }
