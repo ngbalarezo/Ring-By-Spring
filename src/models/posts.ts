4709af38fa0e228162bb6f0e0b0c8c40d4852c3d
Nicholas Balarezo <nbalarezo001@student.@franciscan.edu>
2024-04-07 12:59:42 -0400

    Added user schema content, created comment schema model

--- src/models/posts.ts
+++ src/models/posts.ts
@@ -1,10 +1,13 @@
-import {userID} from "./user";
+import {User} from "./user";
 /*
 Model for posts
 */
 export interface Posts {
+    //postID to identify unique posts
+    postID: number,
     //user represents the id of the user who posted the post
-    userid: userID,
+    //!FIXME: below must be inserted in service/component!
+    userid: number,
     //content is the text of the post
     content: string,
     //timeOfPost says when the post was made
