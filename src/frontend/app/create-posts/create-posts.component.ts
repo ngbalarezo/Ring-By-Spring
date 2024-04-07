c0a4d11ec9dad6df20a3339402f2e8281dd53e4c
Martinez, Joernie <jmartinez005@student.franciscan.edu>
2024-04-07 14:21:03 -0400

    added create posts component logic. made postID string instead of number

--- src/frontend/app/create-posts/create-posts.component.ts
+++ src/frontend/app/create-posts/create-posts.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PostsService } from '../services/posts.service';
+import { Posts } from '../../../models/posts';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'ng-create-posts',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-posts.component.html',
   styleUrl: './create-posts.component.css'
 })
 export class CreatePostsComponent {
+
+  constructor(private router: Router, private postService: PostsService) {} //created a manual route to see the component
+
+  formData = {
+    content: '',
+    image: ''
+  }
+  createPost(): void {
+    // get current date and time
+    const timeOfPost = new Date().toISOString(); //gets current date and time for new posts
+    
+    // create new post object
+    const newPost: Posts = {
+        postID: '#username.date', //in order for post id to be unique - userid is already unique and date is basically unique, simple way of making it unique
+        userid: 111,
+        content: this.formData.content,
+        timeOfPost,
+        image: this.formData.image
+    };
+
+    this.postService.createPost(newPost);
+}
+
+
 
 }
