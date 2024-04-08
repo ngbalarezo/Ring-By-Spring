ac50d228ca95ad7e8fcfb5bf91ccde9e276bc9ff
Martinez, Joernie <jmartinez005@student.franciscan.edu>
2024-04-08 18:46:10 -0400

    created list post component

---
+++ src/frontend/app/list-post/list-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-list-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-post.component.html',
+  styleUrl: './list-post.component.css'
+})
+export class ListPostComponent {
+
+}
