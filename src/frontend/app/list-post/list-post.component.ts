126b423a4c627df762e08b25a0fd3baa95972257
Martinez, Joernie <jmartinez005@student.franciscan.edu>
2024-04-08 22:54:28 -0400

    added component logic to retrieve Posts array from service

--- src/frontend/app/list-post/list-post.component.ts
+++ src/frontend/app/list-post/list-post.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../services/posts.service';
+import { Posts } from '../../../models/posts';
+import { NgFor } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ng-list-post',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './list-post.component.html',
   styleUrl: './list-post.component.css'
 })
 export class ListPostComponent {
+  
+  constructor(private postsService: PostsService) {}
+  
+  posts: Posts[] = []
+
+  ngOnInit() {
+    this.postsService.getAllPosts().subscribe(posts => {
+      this.posts = posts;
+    });
+  }
+
 
 }
