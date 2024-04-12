c79ffa755629dfa8bac03e6caf6f2a7fb7ff5c40
Robert Zaleski <rzaleski@franciscan.edu>
2024-04-12 06:48:26 -0400

    Serve angular dist files

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -2,6 +2,13 @@ import {Elysia} from 'elysia'
 
 const PORT = process.env['LISTEN_PORT'];
 console.log(`Listening on port ${PORT}`);
+const STATIC_ROOT='./dist/frontend/browser';
+
 new Elysia()
-  .get('/', () => 'Hello Elysia')
+  .get('/*', async (context) => {
+    console.log('Static: ', context.path);
+    const staticFile = Bun.file(`./${STATIC_ROOT}/${context.path}`);
+    const fallBackFile = Bun.file(`./${STATIC_ROOT}/index.html`);
+    return (await staticFile.exists()) ? staticFile : fallBackFile;
+  })
   .listen(PORT);
