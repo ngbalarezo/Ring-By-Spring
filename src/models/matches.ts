4bbb9f59326ae631b084a419dd9fac3640bd21bd
Habert, Jude <jhabert001@student.franciscan.edu>
2024-04-13 20:28:16 -0400

    messing around with rebase

--- src/models/matches.ts
+++ src/models/matches.ts
@@ -2,16 +2,17 @@ import { User } from "./user";
 
 //Represents a match between to prospecting users.
 export interface Matches {
+  
   //Unique ID for each match.
   matchID: number,
 
   //Date when the pair of users matched.
   timeMatched: string,
 
-  //First user in the match. User that first reaches out to the recieving user, the userB.
+  //First user in the match. User that first reaches out to the recieving user, the userB. Should this be UserID instead of whole user?
   userA: User,
 
-  //Second user in the match. User that recieves the match and accepts or rejects it from the userA.
+  //Second user in the match. User that recieves the match and accepts or rejects it from the userA. Same question as above.
   userB: User,
   
 }
