fdc032a24ca47a3902afc74679f2055218f70bc8
Nicholas Balarezo <nbalarezo001@student.@franciscan.edu>
2024-04-25 15:37:06 -0400

    Wrote login, register, and home routes. Also added a redirect if the url subdirectory is empty.

--- src/frontend/app/app.routes.ts
+++ src/frontend/app/app.routes.ts
@@ -3,26 +3,40 @@ import { CreateUserComponent } from './components/create-user/create-user.compon
 import { ListUsersComponent } from './components/list-users/list-users.component';
 import { CreatePostsComponent } from './components/create-posts/create-posts.component';
 import { ListPostComponent } from './components/list-post/list-post.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
   {
     path: 'create-user',
     component: CreateUserComponent,
   },
-  
   {
     path: 'create-post',
     component: CreatePostsComponent,
   },
-
   {
     path: 'list-users',
     component: ListUsersComponent,
   },
-  
   {
     path: 'list-post',
     component: ListPostComponent
-  }
-
+  },
 ];
