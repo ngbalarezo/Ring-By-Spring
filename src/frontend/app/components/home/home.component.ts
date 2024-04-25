93c1dbf4dd4a932ef9c396bc7f6741ba6065e140
Nicholas Balarezo <nbalarezo001@student.@franciscan.edu>
2024-04-25 15:24:20 -0400

    Generated home component.

---
+++ src/frontend/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
