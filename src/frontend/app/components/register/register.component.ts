14d3fb6684d4ee7cb7a6411903a8490b454a8eec
Nicholas Balarezo <nbalarezo001@student.@franciscan.edu>
2024-04-28 17:04:27 -0400

    Added register content and created a password match validation.

--- src/frontend/app/components/register/register.component.ts
+++ src/frontend/app/components/register/register.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { passwordMatchValidator } from '../../shared/password-match.directive';
+
 
 @Component({
   selector: 'ng-register',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    CardModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    NgIf
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm  = this.fb.group({
+    firstName: ['', [Validators.required, Validators.pattern(/^[A-Z][a-z ,.'-]+$/i)]],
+    lastName: ['', Validators.required, Validators.pattern(/^[A-Z][a-z ,.'-]+$/i)],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required]
+  }, {
+    validators: passwordMatchValidator
+  })
+
+  constructor(private fb: FormBuilder){}
+
+  get firstName() { return this.registerForm.controls['firstName']; }
+
+  get lastName() { return this.registerForm.controls['lastName']; }
+
+  get email() { return this.registerForm.controls['email']; }
+
+  get password() { return this.registerForm.controls['password']; }
+
+  get confirmPassword() { return this.registerForm.controls['confirmPassword']; }
 
 }
