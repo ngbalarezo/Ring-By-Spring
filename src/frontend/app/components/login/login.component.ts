acaddc7b7ee33f75dc68927c0dcfe529fd6b68bc
Nicholas Balarezo <nbalarezo001@student.@franciscan.edu>
2024-04-27 20:06:55 -0400

    Organized imports and filled out some html for the login component. Also I downloaded some styling tools such as ng flex and ng icons.

--- src/frontend/app/components/login/login.component.ts
+++ src/frontend/app/components/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'ng-login',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    CardModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    ButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  })
 
+  constructor(private fb: FormBuilder) {}
 }
