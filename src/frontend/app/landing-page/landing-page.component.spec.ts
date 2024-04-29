9dc173a2d0b002d6582f61a3d93937ba2cedd84e
Martinez, Joernie <jmartinez005@student.franciscan.edu>
2024-04-29 10:06:26 -0400

    fixed spec test

--- src/frontend/app/landing-page/landing-page.component.spec.ts
+++ src/frontend/app/landing-page/landing-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { LandingPageComponent } from './landing-page.component';
 
 describe('LandingPageComponent', () => {
@@ -8,7 +8,7 @@ describe('LandingPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LandingPageComponent]
+      imports: [LandingPageComponent, RouterTestingModule]
     })
     .compileComponents();
     
