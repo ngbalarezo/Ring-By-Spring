16dbd6ee791ba00f2026c260db3a9005421b8685
Martinez, Joernie <jmartinez005@student.franciscan.edu>
2024-04-28 22:12:26 -0400

    added main age and changed favicon image

---
+++ src/frontend/app/main-page/main-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-main-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.css'
+})
+export class MainPageComponent {
+
+}
